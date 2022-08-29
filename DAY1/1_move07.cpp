2e3be8f4da098395ba96465e0bcd9c6409a02d89
ecourse <smkang@codenuri.co.kr>
2022-08-29 14:24:43 +0900

    Add files via upload

--- DAY1/1_move07.cpp
+++ DAY1/1_move07.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-// Rule Of 3/5
+// Rule Of 3
+// => 포인터 멤버가 있고, 생성자에서 자원 할당하는 코드가 있다면
+//    반드시 3개의 특별 멤버 함수를 더 만들어야 한다.
+// => 소멸자, 복사 생성자, 대입 연산자
+
+// Rule Of 5
+// => C++11 부터는 move 생성자와 move 대입도 만들어라. 
+
+
 class People
 {
 	char* name;
@@ -29,6 +37,26 @@ public:
 
 		name = new char[strlen(p.name) + 1];
 		strcpy_s(name, strlen(p.name) + 1, p.name);
+
+		return *this;
+	}
+	
+	// move 생성자
+	People(People&& p) : name(p.name), age(p.age)
+	{
+		p.name = nullptr;
+	}
+
+	People& operator=(People&& p)
+	{
+		if (&p == this) return *this;
+
+		age = p.age;
+
+		delete[] name;
+		
+		name = p.name;		// 포인터만 복사
+		p.name = nullptr;	// 원래 객체가 자원 포기!
 
 		return *this;
 	}
