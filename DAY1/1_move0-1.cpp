8fe19e1b9c44ef88c8fe6386fa2748f3fa6e53fd
ecourse <smkang@codenuri.co.kr>
2022-08-29 11:39:03 +0900

    Add files via upload

--- DAY1/1_move0-1.cpp
+++ DAY1/1_move0-1.cpp
@@ -4,15 +4,20 @@
 class Point {};
 
 // reference 와 overloading 규칙
-void fn(Point& p)       { std::cout << "Point&" << std::endl; }
-void fn(const Point& p) { std::cout << "const Point&" << std::endl; }
-void fn(Point&& p)		{ std::cout << "Point&&" << std::endl; }
+void fn(Point& p)       { std::cout << "Point&" << std::endl; }		 // 1
+void fn(const Point& p) { std::cout << "const Point&" << std::endl; }// 2
+void fn(Point&& p)		{ std::cout << "Point&&" << std::endl; }	 // 3
 
 int main()
 {
 	Point pt;
 
-	fn(pt);
+	fn(pt); // lvalue
+			// 1번 호출, 1번이 없으면 2번 호출
 
-	fn(Point());
+	fn(Point());	// rvalue(temporary)
+					// 3번호출, 3번이 없으면 2번 호출
+
+	// lvalue 와 rvalue 를 모두 받고 싶다. : const Point&
+	// lvalue 와 rvalue 를 따로 처리하고 싶다. : Point&, Point&& 2개함수제공
 }
