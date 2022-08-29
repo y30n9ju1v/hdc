2c2ab00e6563a1b2d4bc143dd3eadca0095bbf69
ecourse <smkang@codenuri.co.kr>
2022-08-29 13:25:51 +0900

    Add files via upload

--- DAY1/1_move03.cpp
+++ DAY1/1_move03.cpp
@@ -13,12 +13,33 @@ public:
 	}
 	~Cat() { delete[] name; }
 
+	// lvalue, rvalue 모두 받을수있다.
 	Cat(const Cat& c) : age(c.age)
 	{
 		name = new char[strlen(c.name) + 1];
 		strcpy_s(name, strlen(c.name) + 1, c.name);
+		std::cout << "copy" << std::endl;
+	}	
+	// 임시객체(rvalue) 전용 복사 생성자를 만들자.
+	// rvalue 만 받을수 있다.
+	Cat(Cat&& c) 
+		: name(c.name), age(c.age) // 1. 포인터 포함 모든 멤버를 얕은복사
+	{
+		// 이제 임시객체는 더이상 자원을 주소를 가지고 있으면
+		// 안됩니다.
+		c.name = nullptr;
+				// => 이코드가 되려면 
+				//    임시객체를 상수성 없이 가리킬수 있어야 합니다.
+		std::cout << "move" << std::endl;
 	}
 };
+// 결론 : 이름있는 객체를 복사 할때와
+//       임시객체를 복사할때 다른 코드를 만들어서 성능향상을 위한것
+
+// 복사(copy) 생성자 : 이름 있는 객체를복사 할때 (깊은 복사)
+// 이동(move) 생성자 : 이름 없는 객체(임시객체)를 복사 할때 (자원의 이동)
+
+
 
 Cat fn()
 {
