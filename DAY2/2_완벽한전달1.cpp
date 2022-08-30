94bce81e52d0acd2dc8652940cbb690a5c88a94a
ecourse <smkang@codenuri.co.kr>
2022-08-30 10:24:38 +0900

    Add files via upload

--- DAY2/2_완벽한전달1.cpp
+++ DAY2/2_완벽한전달1.cpp
@@ -3,9 +3,14 @@
 void foo(int a)  {}
 void goo(int& a) { a = 100; }
 
+// forwarding : 전달 받은 인자를 다른 함수에 다시 전달하는 것.
+// 
+// perfect forwarding(완벽한 전달)
+// => 전달 받은 인자에 어떠한 변화도 없이 그대로 전달하는 것.
+
 // 특정 함수의 성능을 측정하는 함수를 생각해 봅시다.
 template<typename F, typename T>
-void chronometry(F f, T arg)
+void chronometry(F f, const T& arg)
 {
 	// 현재 시간 기록
 	f(arg);	// 함수 호출
