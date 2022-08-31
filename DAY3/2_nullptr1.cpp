41a94e6c5ad0a62891c94d41ef0e0f6a9a5fa793
ecourse <smkang@codenuri.co.kr>
2022-08-31 09:19:12 +0900

    Add files via upload

---
+++ DAY3/2_nullptr1.cpp
@@ -0,0 +1,13 @@
+﻿// ES.47: Use nullptr rather than 0 or NULL - C++11 이후 권장
+
+int main()
+{
+	int* p1 = 0;		// bad
+	int* p2 = nullptr;	// good
+
+	// 장점 1. 가독성이 좋습니다.
+	auto ret = fn();
+
+	if (ret == 0) {}		// 이 코드만으로 ret가 포인터 인지 정수인지 알수 없다.
+	if (ret == nullptr) {}	// ret가 포인터 라고 예측 가능
+}
