4716751b21eaf0d47d8555b416d591b022e5a434
ecourse <smkang@codenuri.co.kr>
2022-08-31 15:17:31 +0900

    Add files via upload

---
+++ DAY3/6_make1.cpp
@@ -0,0 +1,41 @@
+// T44. Use function templates to 
+//      deduce class template argument types
+
+template<typename T> T square(T a)
+{
+	return a * a;
+}
+
+template<typename T> class List
+{
+public:
+	List(int sz, T value) {}
+};
+
+// List 템플릿의 객체를 생성하는 함수템플릿
+template<typename T>
+List<T> make_list(int sz, T value)
+{
+	return List<T>(sz, value);
+}
+
+
+int main()
+{
+	// 다음중 쉬워 보이는 것은 ?
+	square<int>(3);	// 정확한 표기법
+	square(3);		// 타입인자 생략가능, 항상 쉬워 보입니다.
+
+	// 클래스 템플릿의 경우
+	List<int> s1(10, 3);
+//	List      s2(10, 3);	// 생성자를 통한 타입 추론은 C++17 부터 가능합니다
+
+	auto s3 = make_list<int>(10, 3);
+}
+
+
+
+
+
+
+
