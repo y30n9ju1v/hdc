274144f141c26a87f30d809cd6a3adbadc43a8b7
ecourse <smkang@codenuri.co.kr>
2022-08-31 13:22:53 +0900

    Add files via upload

--- DAY3/4_lambda2-1.cpp
+++ DAY3/4_lambda2-1.cpp
@@ -16,7 +16,6 @@ struct IsDivide
 		return n % value == 0;
 	}
 };
-
 int main()
 {
 	std::vector<int> v = { 4,5,6,1,2,3,7,8,9,0 };
@@ -26,7 +25,10 @@ int main()
 	IsDivide f(k); // k의 값을 캡쳐한 단항 함수(객체)
 	bool b = f(10); // 10 이 k(3)의 배수인가 ?
 
+	auto ret1 = std::find_if(v.begin(), v.end(), f);
 
-	auto ret1 = std::find_if(v.begin(), v.end(), "함수 + k");
+
+	// 한번만 사용한다면 아래처럼 임시객체로 전달하세요..
+	auto ret2 = std::find_if(v.begin(), v.end(), IsDivide(k) );
 
 }
