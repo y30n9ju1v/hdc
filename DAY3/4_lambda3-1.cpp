78cb024dcd44177d92b98707fe20911c6a6bde9e
ecourse <smkang@codenuri.co.kr>
2022-08-31 13:43:51 +0900

    Add files via upload

--- DAY3/4_lambda3-1.cpp
+++ DAY3/4_lambda3-1.cpp
@@ -14,5 +14,15 @@ int main()
 
 	// 핵심 1. 람다표현식의 최종 결과는 "임시객체(rvalue)" 입니다.
 	
+	// 핵심 2. 모든 람다 표현식은 다른 타입 입니다.
 
+	auto f1 = [](int a, int b) { return a + b; };
+				// class xxxx{}; xxxx();
+
+	auto f2 = [](int a, int b) { return a + b; };
+				// class yyyy{}; yyyy();
+
+	// f1, f2는 같은 타입일까요 ?? 다른 타입 일까요 ?
+	std::cout << typeid(f1).name() << std::endl;
+	std::cout << typeid(f2).name() << std::endl;
 }
